da47e6207efe4d58dec72834f1dbeb3d4b71fbb8
Martin Schröder <info@martin-schroeder.net>
2023-04-03 12:54:33 +0200

    initial commit

---
+++ README.rst
@@ -0,0 +1,36 @@
+PyScada Modbus Extension
+======================
+
+This is a extension for PyScada to support Modbus devices.
+
+
+What is Working
+---------------
+
+ - Modbus TCP/IP
+ - Modbus RTU
+
+What is not Working/Missing
+---------------------------
+
+ - Documentation
+
+ 
+Installation
+------------
+
+ - pip install pyscada-modbus
+
+
+Contribute
+----------
+
+ - Issue Tracker: https://github.com/pyscada/PyScada-modbus/issues
+ - Source Code: https://github.com/pyscada/PyScada-modbus
+
+
+License
+-------
+
+The project is licensed under the _GNU AFFERO GENERAL PUBLIC LICENSE Version 3 (AGPLv3)_.
+
---
+++ pyscada/modbus/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from pyscada import core
+
+__version__ = "0.8.0"
+__author__ = "Martin Schröder, Camille Lavayssiere"
+__email__ = "team@pyscada.org"
+__description__ = "Modbus extension for PyScada a Python and Django based Open Source SCADA System"
+__app_name__ = "Modbus"
+
+PROTOCOL_ID = 3
+
+parent_process_list = [{'pk': PROTOCOL_ID,
+                        'label': 'pyscada.modbus',
+                        'process_class': 'pyscada.modbus.worker.Process',
+                        'process_class_kwargs': '{"dt_set":30}',
+                        'enabled': True}]
