fa6deecfbdf7cd150df356f96339c8e600e9823c
clavay <clavayssiere@univ-pau.fr>
2023-04-04 17:25:01 +0200

    fix modbus DeviceProtocol
    update is better than delete and create

--- pyscada/modbus/migrations/0009_add_device_protocol.py
+++ pyscada/modbus/migrations/0009_add_device_protocol.py
@@ -10,16 +10,14 @@ def forwards_func(apps, schema_editor):
     # if we directly import it, it'll be the wrong version
     DeviceProtocol = apps.get_model("pyscada", "DeviceProtocol")
     db_alias = schema_editor.connection.alias
-    DeviceProtocol.objects.using(db_alias).filter(protocol='modbus').delete()
-    DeviceProtocol.objects.using(db_alias).bulk_create([
-        DeviceProtocol(pk=PROTOCOL_ID,
-                       protocol='modbus',
-                       description='Modbus Device',
-                       app_name='pyscada.modbus',
-                       device_class='pyscada.modbus.device',
-                       daq_daemon=True,
-                       single_thread=True),
-    ])
+    DeviceProtocol.objects.using(db_alias).update_or_create(protocol='modbus',
+                                                            defaults={'pk': PROTOCOL_ID,
+                                                                      'description': 'Modbus Device',
+                                                                      'app_name': 'pyscada.modbus',
+                                                                      'device_class': 'pyscada.modbus.device',
+                                                                      'daq_daemon': True,
+                                                                      'single_thread': True}
+                                                            )
 
 
 def reverse_func(apps, schema_editor):
