0c13f743e079485d7f1dbf19638ddf91cdf8deac
clavay <clavayssiere@univ-pau.fr>
2023-04-05 11:43:21 +0200

    add django AppConfig path

--- pyscada/modbus/apps.py
+++ pyscada/modbus/apps.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
@@ -10,6 +11,7 @@ from . import __app_name__
 class PyScadaModbusConfig(AppConfig):
     name = 'pyscada.' + __app_name__.lower()
     verbose_name = _("PyScada " + __app_name__ + " Master/Client")
+    path = os.path.dirname(os.path.realpath(__file__))
     default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
