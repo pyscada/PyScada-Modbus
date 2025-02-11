981c0be74cb06272bdd114c7be08af5ef35c21bf
clavay <clavayssiere@univ-pau.fr>
2025-02-11 11:47:45 +0100

    remove unused import

--- pyscada/modbus/__init__.py
+++ pyscada/modbus/__init__.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from pyscada import core
-
 __version__ = "0.8.0"
 __author__ = "Martin Schröder, Camille Lavayssiere"
 __email__ = "team@pyscada.org"
